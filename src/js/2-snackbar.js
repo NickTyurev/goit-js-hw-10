4518fa092fd81306784549ea98f7c89ada86e79e
Nick_Tyurev <nickverysmart@gmail.com>
2024-10-23 14:43:59 +0300

    hw10 done

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,37 @@
+const form = document.querySelector('.form');
+
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+
+  const delay = parseInt(form.elements.delay.value);
+  const state = form.elements.state.value;
+
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then((delay) => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight'
+      });
+    })
+    .catch((delay) => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight'
+      });
+    });
+});
